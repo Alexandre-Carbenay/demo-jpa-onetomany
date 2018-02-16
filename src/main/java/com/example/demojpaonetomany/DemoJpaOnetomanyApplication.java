bb6a970118ec07ce0543199dc4daaaa0aa65f7ae
Alexandre Carbenay <acarbenay@cirb.brussels>
2018-02-16 16:03:49 +0100

    Initial commit

---
+++ src/main/java/com/example/demojpaonetomany/DemoJpaOnetomanyApplication.java
@@ -0,0 +1,12 @@
+package com.example.demojpaonetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaOnetomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaOnetomanyApplication.class, args);
+	}
+}
